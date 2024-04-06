c2a47b3cd6fc7fd5517f4562477449431dbc2871
Vicentem <vicentealvescabral80@gmail.com>
2024-04-05 21:51:16 -0300

    Envio com arquivos mais banco de dados atualizado

--- backend/drp03_pi/settings.py
+++ backend/drp03_pi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3jfg-zccyiz3o6gi^kb*_p+=d3_os8aw48qo-xf7bx&_qwt60e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'drp03_pi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'bd_biblioteca.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         #'ENGINE': 'django.db.backends.mysql',
         #'NAME': 'bd_biblioteca',
         #'USER': 'drp03-pi',
