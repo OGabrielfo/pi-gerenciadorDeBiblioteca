e38493d1f50c98c820c11513bbfc4fdcc5c962d5
izaazgood <72265851+izaazgood@users.noreply.github.com>
2024-05-02 18:23:23 -0300

    LOGIN
    Consegui acertar o HTML para servir no JS, exceto o wrapper do checkbox. tá dando um erro que não sei como concertar.
    Tirei a cor aquamarine do global pq tava ficando na página. Outra coisa que eu gostaria de remover é o navbar. Ele tá entrando na página de login. CSS tá meio zoado, n sei sé é por isso.
    Falta agora adicionar a imagem e arrumar o CSS (e o erro da checkbox)

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -2,8 +2,29 @@ import styles from './login.module.css'
 
 export default function login() {
     return(
-        <div>
-            <h1>Página de Login</h1>
+      <body className={styles.body}>
+      <div className={styles.logo}>
+        <img src="/LOGO.png" alt="logo" />
+      </div>
+      <div className={styles.container-login}>
+        <div className={styles.login}>
+          <h1>Login</h1>
+          <div className={styles.inputs}>
+            <input type="text" placeholder="Digite seu usuário" className="input-text" />
+            <input type="password" placeholder="Digite sua senha" className="input-password" />
+          </div>
+          <label className="checkbox-label">
+         <div className={styles.checkbox-wrapper-14}>
+              <input id="s1-14" type="checkbox" className="switch" />
+              <label for="s1-14">Permanecer Conectado</label>
+          </div>
+          </label>
+          <div className={styles.botao}>
+            <button>Entrar</button>
+          </div>
+          <br />
         </div>
-    )
+      </div>
+    </body>
+  );
 }
