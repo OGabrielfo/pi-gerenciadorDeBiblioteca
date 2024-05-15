c417b85a8ddb866eeb6ab2bcd39cd8efc5554004
OGabrielfo <ogabrielfo.silva@gmail.com>
2024-05-15 20:03:03 -0300

    Correções da API
    Correção nos arquivos 'urls.py' e 'settings.py'

--- backend/drp03_pi/settings.py
+++ backend/drp03_pi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'biblioteca',
     'bootstrap4',
     'stdimage',
+    'corsheaders'
 
 ]
 
@@ -52,6 +53,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'drp03_pi.urls'
