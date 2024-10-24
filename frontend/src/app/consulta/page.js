2a792f1312c2d813d967ba5e6d0b7356948840d7
andersonjr1 <andersonpaulojr1@gmail.com>
2024-10-24 16:17:09 -0300

    add
    Modal da reserva adicionado para a tela de consulta.

--- frontend/src/app/consulta/page.js
+++ frontend/src/app/consulta/page.js
@@ -59,30 +59,33 @@ const Consulta = () => {
   }
 
   return (
-    <div>
-      <Header>Consulta</Header>   
-      <div className={styles.container}>
-        <div className={styles.title}>
-          <label className={styles.label}>Título</label>
-          <input className={styles.input} type="text" placeholder="Digite o título do livro" value={nomeLivro} onChange={(e) => setNomeLivro(e.target.value)} />
+    <>
+      <div>
+        <Header>Consulta</Header>   
+        <div className={styles.container}>
+          <div className={styles.title}>
+            <label className={styles.label}>Título</label>
+            <input className={styles.input} type="text" placeholder="Digite o título do livro" value={nomeLivro} onChange={(e) => setNomeLivro(e.target.value)} />
+          </div>
+          <div className={styles.aut}>
+            <label className={styles.label}>Autor</label>
+            <input className={styles.input} type="text" placeholder="Digite o autor do livro" value={autor} onChange={(e) => setAutor(e.target.value)} />
+          </div>
+          <div className={styles.gen}>
+            <label className={styles.label}>Gênero</label>
+            <input className={styles.input} type="text" placeholder="Digite o gênero do livro" value={genero} onChange={(e) => setGenero(e.target.value)} />
+          </div>
+          <div className={styles.botao}>
+            <br/>
+            <button className={styles.button} onClick={handleSubmit}>Consultar</button>
+          </div>
         </div>
-        <div className={styles.aut}>
-          <label className={styles.label}>Autor</label>
-          <input className={styles.input} type="text" placeholder="Digite o autor do livro" value={autor} onChange={(e) => setAutor(e.target.value)} />
-        </div>
-        <div className={styles.gen}>
-          <label className={styles.label}>Gênero</label>
-          <input className={styles.input} type="text" placeholder="Digite o gênero do livro" value={genero} onChange={(e) => setGenero(e.target.value)} />
-        </div>
-        <div className={styles.botao}>
-          <br/>
-          <button className={styles.button} onClick={handleSubmit}>Consultar</button>
+        <div className={styles.tabela}>
+          <TabelaConsultar dados={dados}/> 
         </div>
       </div>
-      <div className={styles.tabela}>
-        <TabelaConsultar dados={dados}/> 
-      </div>
-    </div>
+      <div> steste</div>
+    </>
   );
 };
 
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,13 @@
 'use client'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, createContext} from 'react';
 import styles from '@/app/consulta/consulta.module.css';
 import Header from '@/components/header';
 import TabelaConsultar from '@/components/tabelaConsultar';
+import Modal from '@/components/modalReserva'
 
 const API_URL = 'http://127.0.0.1:8000/api/livro/';
+
+export const ReservarLivroContexto = createContext();
 
 export default function Home() {
   const [nomeLivro, setNomeLivro] = useState('');
@@ -12,6 +15,9 @@ export default function Home() {
   const [genero, setGenero] = useState('');
   const [dados, setDados] = useState(null);
   const [dadosAPI, setDadosAPI] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [registro, setRegistro] = useState([]);
+  const [reserva, setReserva] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -49,29 +55,62 @@ export default function Home() {
   };
 
   return (
+    <>
     <div>
-      <Header PublicNav={true}>Consulta</Header>
-      <div className={styles.container}>
-        <div className={styles.title}>
-          <label className={styles.label}>Título</label>
-          <input className={styles.input} type="text" placeholder="Digite o título do livro" value={nomeLivro} onChange={(e) => setNomeLivro(e.target.value)} />
+        <Header PublicNav={true}>Consulta</Header>
+        <div className={styles.container}>
+          <div className={styles.title}>
+            <label className={styles.label}>Título</label>
+            <input className={styles.input} type="text" placeholder="Digite o título do livro" value={nomeLivro} onChange={(e) => setNomeLivro(e.target.value)} />
+          </div>
+          <div className={styles.aut}>
+            <label className={styles.label}>Autor</label>
+            <input className={styles.input} type="text" placeholder="Digite o autor do livro" value={autor} onChange={(e) => setAutor(e.target.value)} />
+          </div>
+          <div className={styles.gen}>
+            <label className={styles.label}>Gênero</label>
+            <input className={styles.input} type="text" placeholder="Digite o gênero do livro" value={genero} onChange={(e) => setGenero(e.target.value)} />
+          </div>
+          <div className={styles.botao}>
+            <br/>
+            <button className={styles.button} onClick={handleSubmit}>Consultar</button>
+          </div>
         </div>
-        <div className={styles.aut}>
-          <label className={styles.label}>Autor</label>
-          <input className={styles.input} type="text" placeholder="Digite o autor do livro" value={autor} onChange={(e) => setAutor(e.target.value)} />
-        </div>
-        <div className={styles.gen}>
-          <label className={styles.label}>Gênero</label>
-          <input className={styles.input} type="text" placeholder="Digite o gênero do livro" value={genero} onChange={(e) => setGenero(e.target.value)} />
-        </div>
-        <div className={styles.botao}>
-          <br/>
-          <button className={styles.button} onClick={handleSubmit}>Consultar</button>
+        <div className={styles.tabela}>
+        <ReservarLivroContexto.Provider value={{showModal, setShowModal, registro, setRegistro}}>
+          <TabelaConsultar dados={dados} publico={true}/> 
+        </ReservarLivroContexto.Provider>
         </div>
       </div>
-      <div className={styles.tabela}>
-        <TabelaConsultar dados={dados} publico={true}/> 
-      </div>
-    </div>
+      <div>
+        {showModal &&
+            <Modal onClose={() => setShowModal(false)}>
+                <div className={styles.mainModal}>
+                  <div className={styles.tituloModal}>Reservar Livro</div>
+                  <div className={styles.textoModal}>
+                    <div>
+                      <span>Nome da pessoa: </span>
+                      <span id="modalTitulo">{registro.nome_do_livro}</span>
+                    </div>
+                    <div>
+                      <span>Email: </span>
+                      <span id="modalAutor">{registro.autor}</span>
+                    </div>
+                    <div>
+                      <span>Nome do gênero: </span>
+                      <span id="modalGenero">{registro.tipo}</span>
+                    </div>
+                  </div>
+                  <div className={styles.botoesMainModal}>
+                    <button className={styles.btReserva + " " + styles.btGenerico}>Efetivar Reserva</button>
+                  </div>
+                  <div id="msgModal" className={styles.msgModal}></div>
+                </div>
+            </Modal>
+        }
+        </div>
+      <div id="modal-root"></div>
+    </>
+
   );
 };
